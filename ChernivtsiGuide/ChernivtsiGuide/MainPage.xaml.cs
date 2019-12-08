f354167db3a642aaa3f499e8e6abe30ec83df6ba
nedelko <valentynnedelko@gmail.com>
2019-12-08 20:34:53 +0200

    Add generalTypes

--- ChernivtsiGuide/ChernivtsiGuide/MainPage.xaml.cs
+++ ChernivtsiGuide/ChernivtsiGuide/MainPage.xaml.cs
@@ -3,9 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.StyleSheets;
 
 namespace ChernivtsiGuide
 {
@@ -14,34 +16,46 @@ namespace ChernivtsiGuide
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        Label placeQuestion = new Label() { Text = "general"};
         public MainPage()
         {
-            List<Label> labels = new List<Label>();
+            this.Resources.Add(StyleSheet.FromAssemblyResource
+                (IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly,
+            "ChernivtsiGuide.mystyles.css"));
+
             Question generalQuestion = App.questionRepository.GetQuestion(1);
             Label generalQuestionLebel = new Label() { Text = generalQuestion.Question_content };
+
             IEnumerable<General_type> general_Types = App.generalTypeRepository.GetItems();
-            StackLayout stackLayout1 = new StackLayout();            
+            StackLayout generalTypesStack = new StackLayout();
             foreach (General_type general_Type in general_Types) {
-                stackLayout1.Children.Add(new Label() { Text = general_Type.General_name });
-                stackLayout1.Children.Add(new Label() { Text = general_Type.General_name });
-                stackLayout1.Children.Add(new Label() { Text = general_Type.General_name });
-                stackLayout1.Children.Add(new Label() { Text = general_Type.General_name });
+                Button generalButton = new Button() { Text = general_Type.General_name, WidthRequest = 200, HeightRequest = 50};
+                generalButton.Clicked += delegate { OnButtonClicked(general_Type.General_id); };
+                generalTypesStack.Children.Add(generalButton);
             }
-            ScrollView scrollView = new ScrollView() {
-                Content = stackLayout1,
-                Orientation = ScrollOrientation.Vertical
+            generalTypesStack.Orientation = StackOrientation.Horizontal;
+
+            ScrollView generalTypeScrollView = new ScrollView()
+            {
+                Content = generalTypesStack,
+                Orientation = ScrollOrientation.Horizontal,
             };
+
             StackLayout stackLayout = new StackLayout()
             {
-                Children = {generalQuestionLebel}
+                Children = { generalQuestionLebel, generalTypeScrollView, generalQuestionLebel, placeQuestion }
             };
-            stackLayout1.Orientation = StackOrientation.Horizontal;
+            
             this.Content = new ScrollView()
             {
                 HorizontalOptions = LayoutOptions.Fill,
-                Orientation = ScrollOrientation.Horizontal,
-                Content = stackLayout1
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Orientation = ScrollOrientation.Vertical,
+                Content = stackLayout
             };
+        }
+        public static void OnButtonClicked(int id) {
+            Console.WriteLine("Hi. You clicked {0}", id);
         }
     }
 }
